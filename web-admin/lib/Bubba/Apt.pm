079ace1eefa67edc4a67c682ec8a42d23da76eee
Carl Fürstenberg <carl@excito.com>
2011-06-14 16:18:46 +0200

    Change status file descriptor
    Due to strange funky bugs, the use of file descriptor 1 doesnt' always
    work (neither does FD 2 by the way). Thus changing the file descriptor
    to 6 (arbitary choosen).
    
    Sadly the normal perlopen procedure didn't want to do what I wanted to
    do, so switching to IPC::Run. Might want to remove usage of IPC::Run3
    now as well.
    
    Above should work™

--- web-admin/lib/Bubba/Apt.pm
+++ web-admin/lib/Bubba/Apt.pm
@@ -12,6 +12,7 @@ use threads::shared;
 use base qw(Net::Daemon);
 use Parse::DebControl;
 use IPC::Run3;
+use IPC::Run qw( start pump finish timeout );
 use XML::LibXML;
 use Try::Tiny;
 
@@ -539,6 +540,8 @@ sub install_package {
 }
 
 sub upgrade_packages {
+	$ENV{'DEBIAN_FRONTEND'} = 'noninteractive';
+
 	my ($self, $req) = @_;
 
 	{
@@ -554,14 +557,17 @@ sub upgrade_packages {
 		$max_level = 2;
 	}
 
-	open APT, '-|', "DEBIAN_FRONTEND=noninteractive /usr/bin/bubba-apt --config-file=/etc/apt/bubba-apt.conf update";
-	while ( <APT> ) {
-		last if $self->_process_line( $_, 0, 40 );
+
+	my ($h, @cmd, $statusfd);
+
+	@cmd = qw(/usr/bin/bubba-apt --config-file=/etc/apt/bubba-apt.conf update);
+
+	$h = start \@cmd, '6>', \$statusfd;
+	while($h->pump) {
+		$self->_process_line( $statusfd, 0, 40 );
+		$statusfd = '';
 	}
-	close APT || do {
-		$self->_handle_error( "'bubba-apt update' exited $?" );
-		return;
-	};
+	$h->finish();
 
 	{
 		$main_status = 'Upgrade: Querying available upgrades';
@@ -588,15 +594,14 @@ sub upgrade_packages {
 		$level = 0;
 		$max_level = 3;
 	}
-	open APT, '-|', "DEBIAN_FRONTEND=noninteractive /usr/bin/bubba-apt --config-file=/etc/apt/bubba-apt.conf dist-upgrade";
-	while ( <APT> ) {
-		last if $self->_process_line( $_, 40, 60 );
-	}
-	close APT || do {
-		$self->_handle_error( "'bubba-apt update dist-upgrade' exited $?" );
-		return;
-	};
+	@cmd = qw(/usr/bin/bubba-apt --config-file=/etc/apt/bubba-apt.conf dist-upgrade);
 
+	$h = start \@cmd, '6>', \$statusfd;
+	while($h->pump) {
+		$self->_process_line( $statusfd, 40, 60 );
+		$statusfd = '';
+	}
+	$h->finish();
 }
 
 sub query_progress {
